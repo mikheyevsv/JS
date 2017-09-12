356a5015736c7eaf4aa67431577a65465795ff0b
Mikheyevsv <mikheyevsv@gmail.com>
2017-09-12 16:55:06 +0300

    calc

--- calc/js/scripts.js
+++ calc/js/scripts.js
@@ -1,40 +1,32 @@
 window.onload = function(e){
-    
-    var sum = document.querySelector('input[name=calc]');
-    var deduct = document.querySelector('input[name=deduct]');
-    var mult = document.querySelector("input[name=mult]");
-    var divid = document.querySelector("input[name=divid]");
-    var inp1 = document.querySelector('input[name=num1]');
-    var inp2 = document.querySelector('input[name=num2]');
-    var span = document.querySelector('.res');
-    
-    sum.onclick = function(){
 
-        var res = parseInt(inp1.value) + parseInt(inp2.value);
-        span.innerHTML = res;
-        sum.setAttribute("disabled", "disabled");
-         }
 
-     deduct.onclick = function(){
+    var buttons = document.querySelectorAll(".calc input[type=button]");
+    for(var i = 0; i < buttons.length; i ++){
+        buttons[i].onclick = calculate;
+    }
 
-        var res = parseInt(inp1.value) - parseInt(inp2.value);
-        span.innerHTML = res;
-        deduct.setAttribute("disabled", "disabled");
-         }
-
-     mult.onclick = function(){
-
-        var res = parseInt(inp1.value) * parseInt(inp2.value);
-        span.innerHTML = res;
-        mult.setAttribute("disabled", "disabled");
-        }
-
-     divid.onclick = function(){
-
-        var res = parseInt(inp1.value) / parseInt(inp2.value);
-        span.innerHTML = res;
-        divid.setAttribute("disabled", "disabled");
-            }
-    
 }
 
+
+function calculate() {
+    var inp1 = document.querySelector('input[name=num1]');
+    var inp2 = document.querySelector('input[name=num2]');
+    var res = document.querySelector('.res');
+    var operation = this.getAttribute("data-value");
+    var result;
+    var res1 = inp1.value;
+    var res2 = inp2.value;
+
+    if(operation === "+"){
+        result = parseInt(res1) + parseInt(res2);
+        operation = this.setAttribute("disabled", "disabled");
+
+
+    } else if (operation === "-"){
+        result = parseInt(res1) - parseInt(res2);
+        operation = this.setAttribute("disabled", "disabled");
+    }
+
+    res.innerHTML = result;
+}
