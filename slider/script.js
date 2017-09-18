b6befac2a30a1f27986d9e2c17f616a53c9b4f6d
Mikheyevsv <mikheyevsv@gmail.com>
2017-09-18 15:45:43 +0300

    slider oop

--- slider/script.js
+++ slider/script.js
@@ -2,45 +2,70 @@
  * Created by AngrySerg on 15.09.2017.
  */
 window.onload = function () {
-    var images1 = document.querySelectorAll(".slider-1 .photos img");
-    var slider1 = new Slider(images1);
+    // var images1 = document.querySelectorAll(".slider-1 .photos img");
+    // var slider1 = new Slider(images1);
+    //
+    // document.querySelector(".slider-1 .buttons .prev").onclick = function () {
+    //     slider1.prev();
+    // }
+    //
+    // document.querySelector(".slider-1 .buttons .next").onclick = function () {
+    //     slider1.next();
+    // }
 
-    document.querySelector(".slider-1 .buttons .prev").onclick = function () {
-        slider1.prev();
-    }
 
-    document.querySelector(".slider-1 .buttons .next").onclick = function () {
-        slider1.next();
-    }
-
-
-    var images2 = document.querySelectorAll(".slider-2 .photos img");
-    var slider2 = new Slider(images2);
-
-   setInterval(function () {
-        slider2.next();
-    }, 2000);
+   //  var images2 = document.querySelectorAll(".slider-2 .photos img");
+   //  var slider2 = new Slider(images2);
+   //
+   // setInterval(function () {
+   //      slider2.next();
+   //  }, 2000);
+    
+    new Slider({
+        images: ".slider-2 .photos img",
+        auto: true,
+        buttonPrev: ".slider-2 .buttons .prev",
+        buttonNext: ".slider-2 .buttons .next",
+        rate: 5000
+    });
+    
 }
 
-function Slider(images) {
-    this.images = images;
+
+function Slider(settings) {
+    this.images = document.querySelectorAll(settings.images);
+    this.auto = settings.auto;
+    this.buttonPrev = settings.buttonPrev;
+    this.buttonNext = settings.buttonNext;
+    this.rate = settings.rate || 1000;
     var i = 0;
+    var slider = this;
+
 
     this.prev = function () {
-        this.images[i].classList.remove("show");
+        slider.images[i].classList.remove("show");
         i--;
         if(i < 0){
-            i = this.images.length - 1;
+            i = slider.images.length - 1;
         }
-        this.images[i].classList.add("show");
+        slider.images[i].classList.add("show");
     }
 
     this.next = function () {
-        this.images[i].classList.remove("show");
+        slider.images[i].classList.remove("show");
         i++;
-        if(i >= this.images.length){
+
+        if(i >= slider.images.length){
+            console.log(i);
             i = 0;
         }
-        this.images[i].classList.add("show");
+        slider.images[i].classList.add("show");
+    }
+
+    document.querySelector(slider.buttonNext).onclick = this.next;
+    document.querySelector(slider.buttonPrev).onclick = this.prev;
+
+    if(slider.auto){
+        setInterval(slider.next, slider.rate);
     }
 }
